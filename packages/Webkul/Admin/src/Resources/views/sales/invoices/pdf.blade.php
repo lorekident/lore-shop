1a727c07318db98eddb2e5f3917e4bd45fc788e7
Julian Tabona <brandontabona@gmail.com>
2024-10-06 20:43:43 +0200

    Change invoice pdf colors

--- packages/Webkul/Admin/src/Resources/views/sales/invoices/pdf.blade.php
+++ packages/Webkul/Admin/src/Resources/views/sales/invoices/pdf.blade.php
@@ -113,7 +113,7 @@
             }
 
             .page-header {
-                border-bottom: 1px solid #E9EFFC;
+                border-bottom: 1px solid #fffbe3;
                 text-align: center;
                 font-size: 24px;
                 text-transform: uppercase;
@@ -155,7 +155,7 @@
             }
 
             table thead th {
-                background-color: #E9EFFC;
+                background-color: #fffbe3;
                 color: #32b440;
                 padding: 6px 18px;
                 text-align: left;
@@ -167,7 +167,7 @@
 
             table tbody td {
                 padding: 9px 18px;
-                border-bottom: 1px solid #E9EFFC;
+                border-bottom: 1px solid #fffbe3;
                 text-align: left;
                 vertical-align: top;
             }
@@ -186,7 +186,7 @@
                 width: 250px;
                 padding-top: 5px;
                 padding-bottom: 5px;
-                background-color: #E9EFFC;
+                background-color: #fffbe3;
                 white-space: nowrap;
             }
 
